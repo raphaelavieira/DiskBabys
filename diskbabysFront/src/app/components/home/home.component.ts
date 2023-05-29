5e0a86a2876279941aab1507b919aa988d41a58d
raphaelavieira <vraphaela09@gmail.com>
2023-05-28 22:51:16 -0300

    Pagina home produto 1.0

--- diskbabysFront/src/app/components/home/home.component.ts
+++ diskbabysFront/src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Product } from 'src/app/models/product';
+import { ProductCrudService } from 'src/app/services/product-crud.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  products$: Observable<Product[]>
+  singleItem$: Observable<Product>;
+  singleItemPic$: Observable<Product>;
+  profilePicture: string;
 
-  constructor() { }
+  constructor(private productCrudService: ProductCrudService, private router: Router) {
+
+  }
 
   ngOnInit(): void {
+    this.singleItem$ = null;
+    this.singleItemPic$ = null;
+    this.products$ = this.productCrudService.fetchAll();
   }
 
 }

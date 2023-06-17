669ab93765c17046f51f6dc38a3f399b3e856e60
dayanertz <dayratzhe@gmail.com>
2023-06-17 18:27:05 -0300

    swagger

--- diskBabysBack/index.js
+++ diskBabysBack/index.js
@@ -5,19 +5,22 @@ const Routes = require('./routes/routes');
 const app = express();
 const PORT = 3000;
 
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 // Configuração do body-parser para lidar com o tamanho do payload
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers',
-    'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
 
 app.use('/', Routes); 
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
--- diskBabysBack/routes/routes.js
+++ diskBabysBack/routes/routes.js
@@ -15,7 +15,7 @@ router.get('/all', userController.getAllUsers)
 router.post('/register', userController.registerUser)
 router.post('/login', userController.loginUser)
 router.get('/user', userController.getUser)
-router.put('/user', authenticateToken, userController.putUser);
+router.put('/user', authenticateToken, userController.putUser); 
 router.put('/user/picture', authenticateToken, userController.putPicture);
 router.delete('/user/:id', authenticateToken, userController.deleteUser);
 // Rotas do produto
